af3d3131ffafc2441507a9ba5cc111bbc648f99a
ItsMeBrille <herhellum@gmail.com>
2024-03-28 16:24:05 +0100

    temp fix to not all channels showing

--- node_helper.js
+++ node_helper.js
@@ -23,7 +23,7 @@ module.exports = NodeHelper.create({
     // Create URL
     const today = new Date();
     const formattedDate = today.toISOString().split('T')[0];
-    const url = `https://tvguide.vg.no/backend/api/tv-schedule?page=1&perPage=3&date=${formattedDate}&tz=Europe%2FOslo`;
+    const url = `https://tvguide.vg.no/backend/api/tv-schedule?page=1&perPage=50&date=${formattedDate}&tz=Europe%2FOslo`;
 
     // Make request to fetch TV schedule data
     request(url, function (error, response, body) {
